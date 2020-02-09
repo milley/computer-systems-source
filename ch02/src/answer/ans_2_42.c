2f0ae57c2a0129c399d1171aa955d7b5f42f2e9d
milley <milley.gm@gmail.com>
2020-02-09 23:15:15 +0800

    commit for answer 2-42 in ch02

---
+++ ch02/src/answer/ans_2_42.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int div16(int x) {
+  /* Compute bias to be either 0  (x >= 0) or 15 (x < 0) */
+  int bias = (x >> 31) & 0xF;
+  return (x + bias) >> 4;
+}
+
+int main() {
+  int a = 32;
+  printf("%d\n", div16(a));
+
+  a = -32;
+  printf("%d\n", div16(a));
+
+  return 0;
+}
