032f32412388d66be624d43061953d5dcb626c23
milley <milley.gm@gmail.com>
2020-02-14 22:27:56 +0800

    illustrate int to float to int

--- ch02/src/answer/ans_2_54.c
+++ ch02/src/answer/ans_2_54.c
@@ -8,6 +8,7 @@ int main() {
   printf("%x\n", num);
   printf("%d\n", num);
   printf("%f\n", (float)num);
+  printf("%d\n", (int)(float)num);
   printf("%x\n", (int)(float)num);
   printf("%x\n", (int)(double)num);
 
@@ -18,8 +19,32 @@ int main() {
   float f2 = __FLT_MAX__;
   printf("%e\n", f1);
   printf("%f\n", f1);
-  printf("%e\n", f2);
-  printf("%f\n", f2);
+  printf("FLT_MAX = %e\n", f2);
+  printf("FLT_MAX = %f\n", f2);
+
+  int i1 = INT_MAX;
+  printf("INT_MAX = %d\n", i1);
+  double diff = (double)f2 - (double)i1;
+  printf("diff = %f\n", diff);
+
+
+  float m = 16777216;
+  printf("%f\n", m);
+  printf("%f\n", m + 1);
+  printf("===========\n");
+
+  for (int a = 16777210; a < 16777224; a++)
+  {
+    float b = a;
+    int c = b;
+    printf("a=%d c=%d b=0x%08x\n", a, c, *((int *)&b));
+  }
+  for (int a = 2147483520; a < 2147483647; a++)
+  {
+    float b = a;
+    int c = b;
+    printf("a=%d c=%d b=0x%08x\n", a, c, *((int *)&b));
+  }
 
   return 0;
 }
