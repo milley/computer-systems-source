a4b9f9433b0bae2541fd00c8a917eab0c5eb4175
milley <milley.gm@gmail.com>
2020-02-19 22:55:17 +0800

    commit answer 2.75 in ch02

---
+++ ch02/src/answer/ans_2_73.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <assert.h>
+#include <limits.h>
+
+/* Addition that saturates to TMin or TMax */
+int saturating_add(int x, int y) {
+  int sum = x + y;
+  int sig_mask = INT_MIN;
+
+  /**
+   * if x > 0, y > 0 but sum < 0, it's a positive overflow
+   * if x < 0, y < 0 but sum > 0, it's a negative overflow
+   */
+  int pos_over = !(x & sig_mask) && !(y & sig_mask) && (sum & sig_mask);
+  int neg_over = (x & sig_mask) && (y & sig_mask) && !(sum & sig_mask);
+
+  (pos_over && (sum = INT_MAX) || neg_over && (sum = INT_MIN));
+  return sum;
+}
+
+int main() {
+  assert(INT_MAX == saturating_add(INT_MAX, 0x1234));
+  assert(INT_MIN == saturating_add(INT_MIN, -0x1234));
+  assert(0x11 + 0x22 == saturating_add(0x11, 0x22));
+
+  return 0;
+}
