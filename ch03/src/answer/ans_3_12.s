8282505a489ddd8924b5aa04444abef2ba1ce2e1
milley <milley.gm@gmail.com>
2020-03-08 23:02:58 +0800

    commit answer 3.14 in ch03

---
+++ ch03/src/answer/ans_3_12.s
@@ -0,0 +1,19 @@
+	.file	"ans_3_12.c"
+	.text
+	.globl	uremdiv
+	.type	uremdiv, @function
+uremdiv:
+.LFB0:
+	.cfi_startproc
+	movq	%rdi, %rax
+	movq	%rdx, %rdi
+	movl	$0, %edx
+	divq	%rsi
+	movq	%rax, (%rdi)
+	movq	%rdx, (%rcx)
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	uremdiv, .-uremdiv
+	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
+	.section	.note.GNU-stack,"",@progbits
